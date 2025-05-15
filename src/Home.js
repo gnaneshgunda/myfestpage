5d62b83c6a7ea7275d1973db0c783bae5c621d24
gnaneshgunda <gnaneshgunda1606@gmail.com>
2025-05-15 12:08:10 +0530

    changes

--- src/Home.js
+++ src/Home.js
@@ -2,8 +2,12 @@ const Home = () => {
     return (  
 
         <div id="home">
+
+            <div style={{height: "15vh"}}></div>
+            <div id="home-container">
             <h1>Welcome to My Portfolio</h1>
             <p>I'm a web developer with a passion for creating beautiful and functional websites.</p>
+            </div>
         </div>
     );
 }
